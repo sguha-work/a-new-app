3ba4c0fd73dc63901d8130e308a68ad0cc4d613d
sguha-work <sguha1988.life@gmail.com>
2017-01-04 11:01:20 +0000

    added requestService to check every request

--- api/services/RequestService.js
+++ api/services/RequestService.js
@@ -1,12 +1,12 @@
 module.exports = {
     
   /**
-   * @description - This function checks if the request method is GET or not, if GET it returns true as 
+   * @description - This function checks if the request method is POST or not, if other it returns true as 
    * the request mast be blocked
    * 
    * @params request {Object} - The request object which need to be tested
    * 
-   * @return {Boolean} - true if request mast be blocked, false if request is alright
+   * @return {Boolean} - true if request need to be blocked, false if request is alright
    */ 
   restrictIfNotPostMethod: function(request) {
       if (request.method.toUpperCase() === 'POST') {
