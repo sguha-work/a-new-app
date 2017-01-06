5a5df31a1ea9306bf3483ab5a43af799ee08cf19
sguha-work <sguha1988.life@gmail.com>
2017-01-06 12:53:32 +0000

    product api continues

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -1,9 +1,8 @@
+var UserService = require('./scripts/UserScript.js');
+
+UserService.utilities = new UserService.methods();
 module.exports = {
     getUserIdFromUserEmail: function (userEmail) {
-        console.log("get user id");
-        var promise = new Promise(function (resolve, reject) {
-            resolve(1);
-        });
-        return promise;
+        return UserService.utilities.getUserIdFromUserEmail(userEmail);
     }
 }
---
+++ api/services/scripts/UserScript.js
@@ -0,0 +1,11 @@
+var UserService;
+UserService = {};
+UserService.methods = (function () {
+    this.getUserIdFromUserEmail = (function () {
+        var promise = new Promise(function (resolve, reject) {
+            resolve(1);
+        });
+    });
+});
+
+module.exports(UserService);
